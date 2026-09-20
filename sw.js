d984d48b7c2003fccf87fd4868793bba1c4aa9b6
infoznetu-rgb <infoznetu@gmail.com>
2026-09-20 11:11:01 +0200

    Refresh cache for new bench post photos

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
-const CACHE='zahrada-live-v4';
-const ASSETS=['./','./index.html','./zahrada.css','./zahrada.js','./zahrada.webmanifest','./app-icon.svg','./projekty/hojdacia-lavicka.html','./assets/projects/lavicka/hlavna.webp','./assets/projects/lavicka/detail.webp'];
+const CACHE='zahrada-live-v5';
+const ASSETS=[
+  './',
+  './index.html',
+  './zahrada.css',
+  './zahrada.js',
+  './zahrada.webmanifest',
+  './app-icon.svg',
+  './projekty/hojdacia-lavicka.html',
+  './assets/posts/terasova-hojdacia-lavicka/hlavna.webp',
+  './assets/posts/terasova-hojdacia-lavicka/vyroba.webp',
+  './assets/posts/terasova-hojdacia-lavicka/dielna.webp'
+];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
