ac9ea2ccf225e20a6077a53fb76e73209d675648
infoznetu-rgb <infoznetu@gmail.com>
2026-09-21 15:28:15 +0200

    Update admin service worker registration

--- admin/admin-pwa.js
+++ admin/admin-pwa.js
@@ -94,7 +94,7 @@
     });
     window.addEventListener('load',async()=>{
       try{
-        const reg=await navigator.serviceWorker.register('./admin-sw.js?v=5',{scope:'./',updateViaCache:'none'});
+        const reg=await navigator.serviceWorker.register('./admin-sw.js?v=6',{scope:'./',updateViaCache:'none'});
         reg.update().catch(()=>{});
       }catch(e){}
     });
